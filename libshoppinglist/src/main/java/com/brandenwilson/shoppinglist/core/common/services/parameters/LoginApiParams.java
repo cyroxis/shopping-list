38fe22ae2e614dab1ee203047dfb90c7eb435601
Branden Wilson <bwilson@developertown.com>
2015-04-01 16:05:44 -0400

    Added LoginInteractor tests

--- libshoppinglist/src/main/java/com/brandenwilson/shoppinglist/core/common/services/parameters/LoginApiParams.java
+++ libshoppinglist/src/main/java/com/brandenwilson/shoppinglist/core/common/services/parameters/LoginApiParams.java
@@ -2,6 +2,8 @@ package com.brandenwilson.shoppinglist.core.common.services.parameters;
 
 import com.brandenwilson.shoppinglist.core.common.models.User;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class LoginApiParams {
 
     public interface Callback {
@@ -16,5 +18,19 @@ public class LoginApiParams {
         this.email = email;
         this.password = password;
         this.callback = callback;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof LoginApiParams)) {
+            return false;
+        }
+
+        LoginApiParams loginApiParams = (LoginApiParams) obj;
+        return obj == this
+                || callback == loginApiParams.callback
+                && StringUtils.equals(email, loginApiParams.email)
+                && StringUtils.equals(password, loginApiParams.password);
+
     }
 }
---
+++ libshoppinglist/src/test/java/com/brandenwilson/shoppinglist/core/modules/login/TestLoginInteractor.java
@@ -0,0 +1,55 @@
+package com.brandenwilson.shoppinglist.core.modules.login;
+
+import com.brandenwilson.shoppinglist.core.common.models.Credentials;
+import com.brandenwilson.shoppinglist.core.common.models.TestUser;
+import com.brandenwilson.shoppinglist.core.common.models.User;
+import com.brandenwilson.shoppinglist.core.common.services.ShoppingListApi;
+import com.brandenwilson.shoppinglist.core.common.services.parameters.LoginApiParams;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+public class TestLoginInteractor {
+
+    @Mock
+    ShoppingListApi shoppingListApi;
+
+    @Mock
+    LoginInteractor.Callback callback;
+
+    LoginInteractor loginInteractor;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        loginInteractor = new LoginInteractor(shoppingListApi);
+        loginInteractor.setCallback(callback);
+    }
+
+    @Test
+    public void testSetUp() {
+        verifyZeroInteractions(shoppingListApi);
+        verifyZeroInteractions(callback);
+    }
+
+    @Test
+    public void testLogin() {
+        loginInteractor.login(new Credentials("test@test.com", "password"));
+        verify(shoppingListApi).login(eq(new LoginApiParams("test@test.com", "password", loginInteractor)));
+    }
+
+    @Test
+    public void testLoginSucceeded() {
+        User user = new TestUser();
+        loginInteractor.loginSucceeded(user);
+        verify(callback).userLoggedIn(user);
+    }
+}
+
